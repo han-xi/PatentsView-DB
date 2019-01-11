59f5f04dc93e1cc2daf02d6b57ded1106bdc4a9b
CEDeal <cdeal@air.org>
2019-01-11 18:27:28 +0000

    updated template to point to QA files

--- Development/QA/check_db_counts.py
+++ Development/QA/check_db_counts.py
@@ -1,26 +1,30 @@
 #Counts QA
 
 #set up sql alchemy engine
+import pandas as pd
 import os
 import configparser
 import sys
-sys.path.append('{}/{}'.format(os.getcwd(), 'Development'))
-sys.path.append('/usr/local/airflow/PatentsView-DB/Development')
+#sys.path.append('{}/{}'.format(os.getcwd(), 'Development'))
+sys.path.append('/project/Development')
 from helpers import general_helpers
 import configparser
 config = configparser.ConfigParser()
-config.read('/usr/local/airflow/PatentsView-DB/Development/config.ini')
+config.read('/project/Development/config.ini')
 host = config['DATABASE']['HOST']
 username = config['DATABASE']['USERNAME']
 password = config['DATABASE']['PASSWORD']
 new_database = config['DATABASE']['NEW_DB']
 old_database = config['DATABASE']['OLD_DB']
 temporary_upload = config['DATABASE']['TEMP_UPLOAD_DB']
+previous_qa_loc = config['FOLDERS']['OLD_QA_LOC']
+new_qa_loc = config['FOLDERS']['OLD_QA_LOC']
+latest_expected_date = config['CONSTANTS']['LATEST_DATE']
 
-engine = general_helpers.connect_to_db(host, username, password, new_database)
-
-latest_expected_data = '2018-11-27'
-
+#engine = general_helpers.connect_to_db(host, username, password, new_database)
+data = pd.read_excel("{}/other_sum_qa.xlsx".format(previous_qa_loc))
+print (data.head)
+'''
 #Counts- creating count description
 def create_count_description(old_db_count, new_db_count, table, min_inc = 1.01, max_inc = 1.1):
     unchanging = ['cpc_subsection', 'wipo_field', 'nber_category', 'nber', 'cpc_group',
@@ -76,4 +80,4 @@ def make_excel(new_counts, OLD_QA_LOC, NEW_QA_LOC, new_db):
 #Approximately 
 new_counts = get_counts(OLD_QA_LOC, NEW_QA_LOC, new_db)
 make_excel(new_counts,OLD_QA_LOC, NEW_QA_LOC, new_db)
-
+'''
